e385f81ee397bde413db4018197023258eaabd83
Anthonny <heatdkwow@gmail.com>
2023-11-01 09:31:57 -0500

    Integración

---
+++ send.php
@@ -0,0 +1,32 @@
+<?php
+include("conexion.php");
+
+$usuario=$_POST['usuario'];
+$clave=$_POST['clave'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+
+$conexion=mysqli_connect("localhost","root","","formulario");
+
+$consulta="SELECT*FROM login where usuario='$usuario' and clave='$clave'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:index.php");
+
+}else{
+    ?>
+    <?php
+    include("login.php");
+
+  ?>
+  <h1 class="bad" style="color: red;">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+?>
